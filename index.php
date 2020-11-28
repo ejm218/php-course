a73871146fc2f360a797157ea3697a70443bd095
ejm218 <67883042+ejm218@users.noreply.github.com>
2020-11-28 22:32:53 +0100

    Added "what you can afford" section

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			$credit = 1000;
 			echo "<h1>Welcome to ".$name."!</h1>";
 			echo "<h2>You have ".$credit." in your wallet.</h2>";
-			
+
 			$products['Computer']=750;
 			$products['Car']=15000;
 			$products['iPhone']=1000;
@@ -17,7 +17,16 @@
 
 			foreach($products as $key ==> $value){
 				echo "<php>The ".$key." costs ".$value."</p>;
+			
+			echo "<h2>Items you can afford</h2>";
+			
+			foreach($products as $key ==> $value){
+				if($value <= $credit ){
+					echo "<p>".$key."</p>";
+				}
 			}
+			
 		?>
+
 	</body> 
 </html>
